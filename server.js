8c637683303782926275d5a0317d46231ee94937
Shashank457 <mishrashashank457@gmail.com>
2022-04-17 12:37:06 +0530

    first

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const pug = require("pug");
 const path = require("path");
 const app = express();
+const dotenv = require("dotenv");
+dotenv.config({ path: `./.env` });
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "frontend")));
@@ -9,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const userRoute = require("./routes/userRoute");
 app.use("/", userRoute);
-
-app.listen(8000, () => {
+const port = process.env.PORT;
+app.listen(port, () => {
   console.log("server is running at 8000 port");
 });
